6a275c88dfdcdcbde3f28ec527083164811c8302
Adore_ZL <adoreiron@gmail.com>
2019-12-25 10:54:40 +0800

    重新格式化

--- src/test/java/com/boot/placms/AwesomeApplicationTests.java
+++ src/test/java/com/boot/placms/AwesomeApplicationTests.java
@@ -1,17 +1,17 @@
-package com.boot.placms;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class AwesomeApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
-
+//package com.boot.placms;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class AwesomeApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
+//
